d0cd910c934a670eb89ca0f99df93ef1b8dfc0c8
yichil <yichil1217@gmail.com>
2019-12-07 01:17:55 -0500

    Choropleth plots by different countries

---
+++ analysis/choropleth.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(plotly)
+library(countrycode)
+
+yearly_df <- read.csv("/Users/nessyliu/Documents/GitHub/Edav-Final-Project/data/clean/yearly_data.csv", header = T)
+yearly_df <- select(yearly_df,-"X")
+
+yearly_map_df <- subset(yearly_df, Region!="global")
+yearly_map_df$country_code <- countrycode(toupper(yearly_map_df$Region),origin = 'iso2c',destination = 'iso3c')
+
+
+g <- list(
+  showframe = FALSE,
+  showcoastlines = FALSE,
+  projection = list(type = 'Mercator')
+)
+
+plot_df <- aggregate(yearly_map_df[, c("danceability","energy","loudness","speechiness",
+                                       "acousticness","instrumentalness","liveness" ,
+                                       "valence","tempo")], 
+                     list(yearly_map_df$country_code), mean)
+plot_df$country_name <- countrycode(plot_df$Group.1,origin = 'iso3c',destination = 'country.name')
+
+p <- plot_geo(plot_df) %>%
+  add_trace(
+    z = plot_df$instrumentalness, color = plot_df$instrumentalness, colors = 'Blues',
+    text = ~country_name, locations = ~Group.1, marker = list(line = l)
+  ) %>%
+  colorbar(title = '', tickprefix = '') %>%
+  layout(
+    title = 'Average song features by country',
+    geo = g,
+    margin = list(l=30)
+    )
+
+
+
